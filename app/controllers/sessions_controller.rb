ea17d2b6a9b5ad8efa631acd2dbe521a87be53f8
thinkAmi <dev.thinkami+git@gmail.com>
2021-02-11 21:24:17 +0900

    9章完了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,10 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in user
+
+      # チェックボックスの送信結果を処理する
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+
       redirect_to user
     else
       # flashメッセージによる、エラーメッセージの表示
@@ -17,8 +21,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    # セッションを破棄(ログアウト)
-    log_out
+    # ログインしている場合のみ、セッションを破棄(ログアウト)
+    log_out if logged_in?
     redirect_to root_url
   end
 end
