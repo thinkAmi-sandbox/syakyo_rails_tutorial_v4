4e6266ea528aa1a9cdb471e74e37f9863e6b3fae
thinkAmi <dev.thinkami+git@gmail.com>
2021-02-11 20:48:34 +0900

    8章完了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,5 +17,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    # セッションを破棄(ログアウト)
+    log_out
+    redirect_to root_url
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,4 +11,9 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  #
+  # テストユーザーがログイン中のときにtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
